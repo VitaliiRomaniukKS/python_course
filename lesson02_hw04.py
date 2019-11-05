9b36a802f8beefe4dd932f3a985a74bf5ab6205d
VitaliiRomaniukKS <55143604+VitaliiRomaniukKS@users.noreply.github.com>
2019-11-05 07:29:36 +0200

    Delete lesson02_hw04.py

--- lesson02_hw04.py
+++
@@ -1,5 +0,0 @@
-import math
-my_numb = input ('Введите число:')
-my_numb = int(my_numb)
-print ('В в 16ти-ричном представлении число', my_numb, 'будет выглядеть как:', hex(my_numb))
-print ('В в 8ми-ричном представлении число', my_numb, 'будет выглядеть как:', oct(my_numb))
