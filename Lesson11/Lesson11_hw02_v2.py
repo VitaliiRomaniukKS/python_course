c777f283527e2fe254dad34817ff9e950cc581d0
VitaliiRomaniukKS <55143604+VitaliiRomaniukKS@users.noreply.github.com>
2019-12-18 07:57:27 +0200

    Update Lesson11_hw02_v2.py

--- Lesson11/Lesson11_hw02_v2.py
+++ Lesson11/Lesson11_hw02_v2.py
@@ -15,11 +15,9 @@ with open('payments.txt', 'r') as payments_f:
     for line in payments_f:
         new_trans = line.split(';')
         if (len(new_trans) == 5) and (new_trans[-2] == 'out'):
-            # print(new_trans)
             new_trans.remove(new_trans[4])
             out_trans_list.append(new_trans)
 
-# print(out_trans_list)
 payments_d = {}
 for i in out_trans_list:
     summa = float (i[1].split()[0].replace(',','.'))
@@ -28,25 +26,6 @@ for i in out_trans_list:
     else:
         payments_d[i[0]].append(summa)    
 
-# print()
-# print(payments_d)
-
-# max_pay = [0,0]
-# max_sum = [0,0]
-# max_price = [0,0]
-# for name, p_count in payments_d.items():
-#     if len(p_count) > max_pay[1]:
-#         max_pay = [name,len(p_count)]
-#     if sum(p_count) > max_sum[1]:
-#         max_sum = [name, sum(p_count)]
-#     if max(p_count) > max_price[1]:
-#         max_price = [name,max(p_count)]   
-
-# print(max_pay)
-# print(max_sum)
-# print(max_price)
-
-print('************')
 print('Максимальное количество покупок: ', out_by_func(len, payments_d))
 print('Максимальная сумма за все покупки: ', out_by_func(sum, payments_d))
 print('Максимальная цена одной покупоки: ', out_by_func(max, payments_d))
